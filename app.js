02fe404ff12e2cc0bdbd19075682b150f615c97e
tinchoaraneda <martin.araneda@outlook.com>
2024-01-24 20:43:59 -0300

    Trabajo practico numero 1 - Estructura de un sitio web

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app  = express();
+const path = require('path')
+const port = 3000
+
+app.use(express.static(path.join(__dirname,"public")))
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views/home.html')))
+
+app.listen(port,()=>  console.log('Servidor abierto'));
